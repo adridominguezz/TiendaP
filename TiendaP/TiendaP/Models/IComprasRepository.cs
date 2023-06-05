fe90c6ded4a2d75513994808668870336c7fb177
adridominguezz <dominguezmorenoadrian@gmail.com>
2023-06-05 22:05:00 +0200

    05/06 22:00
    Se ven las compras realizadas.
    Falta por agregar la funcionar para crear nuevas compras en el botón de confirmar compra.

---
+++ TiendaP/TiendaP/Models/IComprasRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaP.Models
+{
+    public class IComprasRepository
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection Conn = new SqlConnection("Server=(local); Database=TiendaP; Integrated Security=true");
+            Conn.Open();
+            return Conn;
+        }
+
+
+        public static int Agregar(Compras compra)
+        {
+            int retorno = 0;
+            using (SqlConnection Conn = ObtenerConexion())
+            {
+                string insertQuery = "INSERT INTO Compras (idCliente, CantidadProductos, PrecioCompra) " +
+                                     "VALUES (@idCliente, @CantidadProductos, @PrecioCompra)";
+
+                SqlCommand Comando = new SqlCommand(insertQuery, Conn);
+                Comando.Parameters.AddWithValue("@idCliente", compra.IdCliente);
+                Comando.Parameters.AddWithValue("@CantidadProductos", compra.ProductosCompra);
+                Comando.Parameters.AddWithValue("@PrecioCompra", compra.PrecioCompra);
+
+                retorno = Comando.ExecuteNonQuery();
+                Conn.Close();
+            }
+            return retorno;
+        }
+
+
+        public static List<Compras> ObtenerCompras()
+        {
+            List<Compras> Lista = new List<Compras>();
+
+            using (SqlConnection conexion = ObtenerConexion())
+            {
+                SqlCommand comando = new SqlCommand("Use TiendaP; SELECT idCompra, idCliente, CantidadProductos, PrecioCompra FROM Compras", conexion);
+
+
+                SqlDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Compras compra = new Compras();
+                    compra.IdCompra = reader.GetInt32(0);
+                    compra.IdCliente = reader.GetGuid(1).ToString();
+                    compra.ProductosCompra = reader.GetInt32(2);
+                    compra.PrecioCompra = Convert.ToSingle(reader.GetDouble(3));
+
+
+                    Lista.Add(compra);
+                }
+                conexion.Close();
+
+                return Lista;
+            }
+
+        }
+    }
+
+}
